54df1fcdc285e8da7bc280d401241f4ea5974c37
mstfarhanaakter <farhanashetu.idb@gmail.com>
2025-07-28 23:06:10 +0600

    new

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+$link = mysqli_connect("localhost", "root", "", "ecommerce");
+if(!$link){
+    die("Database not connected");
+}else{
+    // echo "Database connect successfully";
+}
+
+?>
