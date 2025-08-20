bda231a7b59b4b47e948c1ae2b5e60979e38f03b
mstfarhanaakter <farhanashetu.idb@gmail.com>
2025-08-20 09:29:36 +0600

    new

--- includes/topbar.php
+++ includes/topbar.php
@@ -1,7 +1,9 @@
 <!-- Topbar Start -->
-<div class="container-fluid">
-    <div class="row bg-secondary py-1 px-xl-5">
-        <div class="col-lg-6 d-none d-lg-block">
+<div class="container-fluid d-none d-lg-block" id="topbar">
+    <div class="row bg-secondary py-1 px-xl-5 align-items-center">
+
+        <!-- Left: About / Contact / Help / FAQs -->
+        <div class="col-lg-4 d-flex">
             <div class="d-inline-flex align-items-center h-100">
                 <a class="text-body mr-3" href="">About</a>
                 <a class="text-body mr-3" href="">Contact</a>
@@ -9,7 +11,17 @@
                 <a class="text-body mr-3" href="">FAQs</a>
             </div>
         </div>
-        <div class="col-lg-6 text-center text-lg-right">
+
+        <!-- Center: Logo -->
+        <div class="col-lg-4 text-center my-2 my-lg-0">
+            <a href="" class="text-decoration-none">
+                <span class="h4 text-uppercase text-primary bg-dark px-2">Dora</span>
+                <span class="h4 text-uppercase text-dark bg-primary px-2 ml-n1">Mart</span>
+            </a>
+        </div>
+
+        <!-- Right: Sign In / Sign Up / Icons -->
+        <div class="col-lg-4 text-right">
             <div class="d-inline-flex align-items-center">
                 <a href="login.php" class="btn btn-sm btn-warning mr-2">
                     <i class="fas fa-sign-in-alt mr-1"></i> <b>Sign in</b>
@@ -18,39 +30,8 @@
                     <i class="fas fa-user-plus mr-1"></i> <b>Sign up</b>
                 </a>
             </div>
-            <div class="d-inline-flex align-items-center d-block d-lg-none">
-                <a href="" class="btn px-0 ml-2">
-                    <i class="fas fa-heart text-dark"></i>
-                    <span class="badge text-dark border border-dark rounded-circle" style="padding-bottom: 2px;">0</span>
-                </a>
-                <a href="" class="btn px-0 ml-2">
-                    <i class="fas fa-shopping-cart text-dark"></i>
-                    <span class="badge text-dark border border-dark rounded-circle" style="padding-bottom: 2px;">0</span>
-                </a>
-            </div>
         </div>
-    </div>
-    <div class="row align-items-center bg-light py-3 px-xl-5 d-none d-lg-flex">
-    <div class="col-lg-12 text-center">
-        <a href="" class="text-decoration-none">
-            <span class="h4 text-uppercase text-primary bg-dark px-2">Dora</span>
-            <span class="h4 text-uppercase text-dark bg-primary px-2 ml-n1">Mart</span>
-        </a>
-    </div>
-</div>
 
-        <!-- <div class="col-lg-8 col-12 text-left">
-            <form action="">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search for products">
-                    <div class="input-group-append">
-                        <span class="input-group-text bg-transparent text-primary">
-                            <i class="fa fa-search"></i>
-                        </span>
-                    </div>
-                </div>
-            </form>
-        </div> -->
     </div>
 </div>
 <!-- Topbar End -->
