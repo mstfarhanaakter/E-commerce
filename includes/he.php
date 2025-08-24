6c6e58c089b3da93dbf3ff1de583691e6361eca2
mstfarhanaakter <farhanashetu.idb@gmail.com>
2025-08-24 13:16:07 +0600

    new

--- includes/he.php
+++ includes/he.php
@@ -17,6 +17,7 @@
 
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
 
     <!-- Libraries Stylesheet -->
     <link href="lib/animate/animate.min.css" rel="stylesheet">
@@ -24,13 +25,16 @@
 
     <!-- new -->
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        integrity="sha512-" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Customized Bootstrap Stylesheet -->
     <link href="assets/css/style.css" rel="stylesheet">
     <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon"><link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 
+    <!-- new -->
     <link rel="stylesheet" href=".../style.css">
+    <link rel="stylesheet" href=".../products.css">
 
 
 </head>
